30ddb0a5cea4f72d212a90a56fc28116723d4533
Kr\/igneshVictor <krvignesh@codeneuron.com>
2019-11-15 16:23:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/mobile/', function (req, res) {
+    res.sendFile(__dirname + '/mobile/index.html');
+});
+app.get('/desktop/', function (req, res) {
+    res.sendFile(__dirname + '/desktop/index.html');
+});
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
+
+io.on('connection', function (socket) {
+    socket.on('orientation', function (data) {
+        io.emit('orientation', data);
+    });
+});
